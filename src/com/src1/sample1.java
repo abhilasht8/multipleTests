5c18867aba48f4c5800fcb2df45df43925a65c30
casta1 <casta1@corp.castsoftware.com>
2017-04-18 08:36:57 +0200

    added files and folder

---
+++ src/com/src1/sample1.java
@@ -0,0 +1,11 @@
+public class HelloDate {
+    /** Sole entry point to application, as always.  
+     *  Example of a function comment as well.
+     *  @parem args array of strings, ignored here (command line input)
+     *  @return No value is returned
+     */
+    public static void main (String[] args) {
+	System.out.println ("Hello, it's: ");
+	System.out.println(new Date());
+    }
+}
